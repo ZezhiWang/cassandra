cc4e30d320b7d3768db40ef02e8b92f1c7e5e604
ZezhiWang <wanganh@bc.edu>
2019-03-24 15:10:02 -0400

    change ABD Tag from blob into stringa

--- src/java/org/apache/cassandra/service/ABDTag.java
+++ src/java/org/apache/cassandra/service/ABDTag.java
@@ -7,6 +7,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
 import java.io.Serializable;
+import java.nio.charset.CharacterCodingException;
+import java.util.List;
+
+import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.FBUtilities;
 
 import org.slf4j.Logger;
@@ -20,6 +24,12 @@ public class ABDTag implements Serializable{
     public ABDTag(){
         this.logicalTIme = 0;
         this.writerId = FBUtilities.getBroadcastAddressAndPort().toString();
+    }
+
+    public ABDTag(String tagString){
+        String[] tagArray = tagString.split(";");
+        this.logicalTIme = Integer.parseInt(tagArray[0]);
+        this.writerId = tagArray[1];
     }
 
 
@@ -36,62 +46,74 @@ public class ABDTag implements Serializable{
         return this;
     }
 
-    public static ByteBuffer serialize(ABDTag tag) {
-        logger.info(tag.toString());
-        byte[] bytes = null;
-        ByteArrayOutputStream bos = null;
-        ObjectOutputStream oos = null;
-        try {
-            bos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(tag);
-            oos.flush();
-            bytes = bos.toByteArray();
-        } catch (IOException e1){
-            logger.info("IOException");
-        }
-
-        try {
-            if (oos != null) {
-                oos.close();
-            }
-            if (bos != null) {
-                bos.close();
-            }
-        } catch (IOException e1){
-            logger.info("IOException");
-        }
-
-        return ByteBuffer.wrap(bytes);
+    public static String serialize(ABDTag tag) {
+        return tag.toString();
+//        logger.info(tag.toString());
+//        byte[] bytes = null;
+//        ByteArrayOutputStream bos = null;
+//        ObjectOutputStream oos = null;
+//        try {
+//            bos = new ByteArrayOutputStream();
+//            oos = new ObjectOutputStream(bos);
+//            oos.writeObject(tag);
+//            oos.flush();
+//            bytes = bos.toByteArray();
+//        } catch (IOException e1){
+//            logger.info("IOException");
+//        }
+//
+//        try {
+//            if (oos != null) {
+//                oos.close();
+//            }
+//            if (bos != null) {
+//                bos.close();
+//            }
+//        } catch (IOException e1){
+//            logger.info("IOException");
+//        }
+//
+//        return ByteBuffer.wrap(bytes);
     }
 
     public static ABDTag deserialize(ByteBuffer buf) {
-        byte[] bytes = new byte[buf.capacity()];
-        buf.get(bytes, 0, bytes.length);
-
-        ABDTag res = null;
-        ByteArrayInputStream bis = null;
-        ObjectInputStream ois = null;
+        String tagString = "";
         try {
-            bis = new ByteArrayInputStream(bytes);
-            ois = new ObjectInputStream(bis);
-            res = (ABDTag) ois.readObject();
-        } catch (ClassNotFoundException e){
-            logger.info("ClassNotFoundException");
-        } catch (IOException e1){
-            logger.info("IOException");
+            tagString = ByteBufferUtil.string(buf);
+        } catch (CharacterCodingException e){
+            logger.warn("err casting tag {}",e);
         }
 
-        try {
-            if (bis != null)
-                bis.close();
-            if (ois != null)
-                ois.close();
-        } catch (IOException e1){
-            logger.info("IOException");
-        }
-        logger.info(res.toString());
-        return res;
+        if (tagString == "")
+            return new ABDTag();
+
+        return new ABDTag(tagString);
+//        byte[] bytes = new byte[buf.capacity()];
+//        buf.get(bytes, 0, bytes.length);
+//
+//        ABDTag res = null;
+//        ByteArrayInputStream bis = null;
+//        ObjectInputStream ois = null;
+//        try {
+//            bis = new ByteArrayInputStream(bytes);
+//            ois = new ObjectInputStream(bis);
+//            res = (ABDTag) ois.readObject();
+//        } catch (ClassNotFoundException e){
+//            logger.info("ClassNotFoundException");
+//        } catch (IOException e1){
+//            logger.info("IOException");
+//        }
+//
+//        try {
+//            if (bis != null)
+//                bis.close();
+//            if (ois != null)
+//                ois.close();
+//        } catch (IOException e1){
+//            logger.info("IOException");
+//        }
+//        logger.info(res.toString());
+//        return res;
     }
 
     public boolean isLarger(ABDTag other){
@@ -103,6 +125,6 @@ public class ABDTag implements Serializable{
     }
 
     public String toString() {
-        return logicalTIme + " " + writerId;
+        return logicalTIme + ";" + writerId;
     }
 }
